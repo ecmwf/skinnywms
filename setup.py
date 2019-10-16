f6771505627632e6955a349613249076ce1a1a31
Sylvie Lamy-Thepaut <sylvie.lamy-thepaut@ecmwf.int>
2019-10-16 09:41:15 +0100

    0.1.9

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(fname):
     return io.open(file_path, encoding='utf-8').read()
 
 
-version = '0.1.8'
+version = '0.1.9'
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
