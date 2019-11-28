e9995927d477d0a7f8d9c21e3adbc6c31b8647ce
Sylvie Lamy-Thepaut <sylvie.lamy-thepaut@ecmwf.int>
2019-11-28 11:20:21 +0000

    Version 0.3.1

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(fname):
     return io.open(file_path, encoding='utf-8').read()
 
 
-version = '0.3.0'
+version = '0.3.1'
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
