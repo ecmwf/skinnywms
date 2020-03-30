17a302ddc718b5bbfb7aaa110c401c6fefdaf7ae
Sylvie <sylvie.lamy-thepaut@ecmwf.int>
2020-03-30 11:45:22 +0100

    version 0.5.2

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(fname):
     return io.open(file_path, encoding='utf-8').read()
 
 
-version = '0.5.0'
+version = '0.5.2'
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
