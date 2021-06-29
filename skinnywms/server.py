ac71941641adab487e249bb3c82ef7a554b34e9f
erosert <47104514+EduardRosert@users.noreply.github.com>
2021-06-29 22:26:56 +0200

    typehints added

--- skinnywms/server.py
+++ skinnywms/server.py
@@ -6,10 +6,14 @@
 # granted to it by virtue of its status as an intergovernmental organisation nor
 # does it submit to any jurisdiction.
 
+from __future__ import annotations # see PEP 563, python 3.7+
+from typing import TYPE_CHECKING # see PEP 563, python 3.7+
+if TYPE_CHECKING:
+    from skinnywms.datatypes import Availability, Plotter, Styler
+
 import logging
 import os
 import tempfile
-
 
 from skinnywms import errors, protocol
 
@@ -57,7 +61,7 @@ class NoCaching:
 
 
 class WMSServer:
-    def __init__(self, availability, plotter, styler, caching=NoCaching()):
+    def __init__(self, availability:Availability, plotter:Plotter, styler:Styler, caching=NoCaching()):
 
         self.availability = availability
         self.availability.set_context(self)
