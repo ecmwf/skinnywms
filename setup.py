e2e68ec4b181a9984b2dd7327587079aaec2ccac
erosert <47104514+EduardRosert@users.noreply.github.com>
2022-06-07 14:29:34 +0100

    updated version

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     include_package_data=True,
     install_requires=[
-        "ecmwflibs>=0.4.16",
+        "ecmwflibs>=0.4.17",
         "netCDF4", 
         "dask[array]",
         "Magics",
