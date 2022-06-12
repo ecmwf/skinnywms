c485b28e8cfd91a371c6830d141b3bf0dbc64dda
Sylvie Lamy-Thepaut <sylvie.lamy-thepaut@ecmwf.int>
2022-06-12 16:35:35 +0100

    progress

--- skinnywms/plot/magics.py
+++ skinnywms/plot/magics.py
@@ -637,20 +637,21 @@ class Styler(datatypes.Styler):
                 symbol_advanced_table_marker_name_list = ['N_0','N_1','N_2','N_3','N_4',
                 'N_5','N_6','N_7','N_8','N_9'],), 
             "present_weather" : driver.mobs(
-                obs_template_file_name=os.getcwd() + "/skinnywms/templates/present_weather.template",
+                obs_template_file_name=os.getcwd() + "/skinnywms/templates/mosmix.template",
                 obs_size=0.6,
                 obs_ring_size=0.2,
-                obs_distance_apart=0.0,
+                obs_distance_apart=1.0,
                 obs_present_weather_colour = "white"
             ),
             "mosmix" : driver.mobs(
                 obs_template_file_name=os.getcwd() + "/skinnywms/templates/mosmix.template",
                 obs_size=0.4,
                 obs_ring_size=0.2,
-                obs_distance_apart=0.0,
+                obs_distance_apart=1.0,
                 obs_present_weather_colour = "white",
                 obs_identification_colour = "white",
                 obs_station_ring_colour = "white",
+                obs_wind_colour = "white",
             ),
             "thunderstorm_probability" : driver.msymb(
                 legend = "on",
